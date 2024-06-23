8dd4604e9927998ab98d9c7f0d5077261bafe6a1
Yunuz Yunuz <112627091+yunkaa19@users.noreply.github.com>
2024-06-23 15:53:12 +0200

    fixed images.

--- my-app/src/lib/Components/Api/User/register.ts
+++ my-app/src/lib/Components/Api/User/register.ts
@@ -6,20 +6,18 @@ interface RegisterUserCommand {
 }
 
 interface RegisterResponse {
-    UserId: string;
-    Email: string;
-    Role: string;
+    identityID: string;
 }
 
 export async function register(data: RegisterUserCommand): Promise<RegisterResponse> {
     try {
         const response = await post<RegisterResponse>('api/Users/register', data, false);
 
-        if (!response.UserId || !response.Email || !response.Role) {
+        if (response.identityID == null) {
             throw new Error('Invalid registration response');
         }
 
-        return response; // Return the registration response
+        return response;
     } catch (error) {
         console.error('Registration error:', error);
         throw error;
